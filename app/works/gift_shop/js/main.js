243ce8ba6a55340db2e47fa9e50f460541cdf3cb
ihorshto <igorshtogrin2016@gmail.com>
2022-05-25 17:58:59 +0300

    added social links and changed title

---
+++ app/works/gift_shop/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+ $('.menu__btn').on('click', function () {
+  $('.menu__list').toggleClass('menu__list--active');
+ });
+
+ $('.menu__btn').on('click', function () {
+  $('.menu__btn').toggleClass('menu__btn--active');
+ });
+
+});
